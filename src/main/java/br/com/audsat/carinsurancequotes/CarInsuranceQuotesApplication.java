8b28f63e9ac4e78368c85fd2db13a5c6e3284bb7
DeboraMendes <debora.mendes@live.com>
2023-05-19 12:02:33 -0300

    Initial commit

---
+++ src/main/java/br/com/audsat/carinsurancequotes/CarInsuranceQuotesApplication.java
@@ -0,0 +1,13 @@
+package br.com.audsat.carinsurancequotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsuranceQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInsuranceQuotesApplication.class, args);
+	}
+
+}
