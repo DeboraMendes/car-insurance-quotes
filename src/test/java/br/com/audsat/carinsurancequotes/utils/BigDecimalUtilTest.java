4c4cc769a0662457cd4ce272efdd82b79ed3d329
DeboraMendes <debora.mendes@live.com>
2023-05-21 20:30:11 -0300

    :recycle: refactor(TEC-32): [Audsat] Teste de unidade

--- src/test/java/br/com/audsat/carinsurancequotes/utils/BigDecimalUtilTest.java
+++ src/test/java/br/com/audsat/carinsurancequotes/utils/BigDecimalUtilTest.java
@@ -1,17 +1,15 @@
 package br.com.audsat.carinsurancequotes.utils;
 
-import org.junit.jupiter.api.Assertions;
+import br.com.audsat.carinsurancequotes.helper.BigDecimalHelperTest;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class BigDecimalUtilTest {
 
     @Test
     public void ruleOfThree() {
         final BigDecimal amount = BigDecimalUtil.ruleOfThree(BigDecimal.valueOf(15000), BigDecimal.valueOf(10));
-        Assertions.assertEquals(0, BigDecimal.valueOf(1500).compareTo(amount));
+        BigDecimalHelperTest.assertEquals(BigDecimal.valueOf(1500), amount);
     }
 }
