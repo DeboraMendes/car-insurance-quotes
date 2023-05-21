49b6b9882f7bc7d4bbf170f967fb5c911799784c
DeboraMendes <debora.mendes@live.com>
2023-05-21 17:33:42 -0300

    :books: docs(TEC-35): [Audsat] Documento de APIs com OpenAPI 3.0

--- src/main/java/br/com/audsat/carinsurancequotes/CarInsuranceQuotesApplication.java
+++ src/main/java/br/com/audsat/carinsurancequotes/CarInsuranceQuotesApplication.java
@@ -1,12 +1,13 @@
 package br.com.audsat.carinsurancequotes;
 
 import br.com.audsat.carinsurancequotes.configurations.business.InsuranceRiskCalculatorConfiguration;
+import br.com.audsat.carinsurancequotes.configurations.openapi.OpenAPIConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {InsuranceRiskCalculatorConfiguration.class})
+@EnableConfigurationProperties(value = {InsuranceRiskCalculatorConfiguration.class, OpenAPIConfigurationProperties.class})
 public class CarInsuranceQuotesApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/audsat/carinsurancequotes/domains/dto/CustomerDTO.java
+++ src/main/java/br/com/audsat/carinsurancequotes/domains/dto/CustomerDTO.java
@@ -1,12 +1,20 @@
 package br.com.audsat.carinsurancequotes.domains.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "Cliente do orçamento")
 public class CustomerDTO {
+
+    @Schema(description = "Identificação do cliente.", example = "1")
     private Long id;
+
+    @Schema(description = "Nome do cliente.", example = "Renata Maldonado")
     private String name;
+
+    @Schema(description = "Dados de motorista do cliente.")
     private DriverDTO driver;
 }
